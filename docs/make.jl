4bbe64dbda79fa4efc0e995a479eb283c8a57d40
Christopher Tessum <ctessum@gmail.com>
2022-04-28 14:11:01 -0500

    Remove MTK from name

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,16 @@
-using GasChemMTK
+using GasChem
 using Documenter
 
-DocMeta.setdocmeta!(GasChemMTK, :DocTestSetup, :(using GasChemMTK); recursive=true)
+DocMeta.setdocmeta!(GasChem, :DocTestSetup, :(using GasChem); recursive=true)
 
 makedocs(;
-    modules=[GasChemMTK],
+    modules=[GasChem],
     authors="EarthSciML authors and contributors",
-    repo="https://github.com/EarthSciML/GasChemMTK.jl/blob/{commit}{path}#{line}",
-    sitename="GasChemMTK.jl",
+    repo="https://github.com/EarthSciML/GasChem.jl/blob/{commit}{path}#{line}",
+    sitename="GasChem.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://EarthSciML.github.io/GasChemMTK.jl",
+        canonical="https://EarthSciML.github.io/GasChem.jl",
         assets=String[],
     ),
     pages=[
@@ -19,5 +19,5 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/EarthSciML/GasChemMTK.jl",
+    repo="github.com/EarthSciML/GasChem.jl",
 )
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
-using GasChemMTK
+using GasChem
 using Test, SafeTestsets
 
-@testset "GasChemMTK.jl" begin
+@testset "GasChem.jl" begin
     @safetestset "SuperFast" begin include("superfast_test.jl") end
 end
