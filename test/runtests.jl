3444472a45804b319aef4b0617b8373177e2a751
Jialin Liu <jialinl612@gmail.com>
2023-01-26 00:09:53 +0800

    JuliaFormatter

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,13 @@ using GasChem
 using Test, SafeTestsets
 
 @testset "GasChem.jl" begin
-    @safetestset "SuperFast" begin include("superfast_test.jl") end
-    @safetestset "FastJX" begin include("fastjx_test.jl") end
-    @safetestset "compose_fastjx_superfast" begin include("compose_fastjx_superfast_test.jl") end
+    @safetestset "SuperFast" begin
+        include("superfast_test.jl")
+    end
+    @safetestset "FastJX" begin
+        include("fastjx_test.jl")
+    end
+    @safetestset "compose_fastjx_superfast" begin
+        include("compose_fastjx_superfast_test.jl")
+    end
 end
