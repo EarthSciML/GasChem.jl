963057e37e712206eabec243f105ef6839e6e5b9
jialinl6 <jialinl6@illinois.edu>
2024-03-13 16:35:41 -0700

    Add Emission test

--- test/Emission_test.jl
+++ test/Emission_test.jl
@@ -1,9 +1,9 @@
 using GasChem
 using Test, EarthSciData, Dates, ModelingToolkit, OrdinaryDiffEq, DifferentialEquations
 
-# @testset "emission" begin
-#     @parameters t
-#     composed_ode = SuperFast(t) + FastJX(t) + Emission(t)
-#     sys = structural_simplify(get_mtk(composed_ode))
-#     @test length(states(sys)) ≈ 18
-# end
+@testset "emission" begin
+    @parameters t
+    composed_ode = SuperFast(t) + FastJX(t) + Emission(t)
+    sys = structural_simplify(get_mtk(composed_ode))
+    @test length(states(sys)) ≈ 18
+end
