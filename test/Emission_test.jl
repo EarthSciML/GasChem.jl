4aa05c67b35d846fa3bf20b70454c35662f5726f
jialinl6 <jialinl6@illinois.edu>
2024-04-01 02:05:28 -0500

    Update unit test

--- test/Emission_test.jl
+++ test/Emission_test.jl
@@ -8,13 +8,9 @@ using Test, Dates, ModelingToolkit, OrdinaryDiffEq, DifferentialEquations, Earth
     tspan = (start, start+3600*24*3)
 
     composed_ode = SuperFast(t) + FastJX(t) + Emission(t)
-    sf = SuperFast(t)
     sys = structural_simplify(get_mtk(composed_ode))
-    sys2 = structural_simplify(get_mtk(sf))
     @test length(states(sys)) ≈ 18
     
     sol = solve(ODEProblem(structural_simplify(get_mtk(composed_ode)), [], tspan, []),AutoTsit5(Rosenbrock23()), saveat=10.0)
-    sol2 = solve(ODEProblem(structural_simplify(get_mtk(sf)), [], tspan, []),AutoTsit5(Rosenbrock23()), saveat=10.0)
-    @test sol != sol2
-    @test sol[end][end] ≈ 1.3654736679144752
+    @test isapprox(sol[end][end], 1.3654736679144752, atol = 0.001)
 end
