b8d8b1f9895dd3f55bc60b37534cc971e0494b07
jialinl6 <jialinl6@illinois.edu>
2024-04-15 15:09:46 -0500

    Error during solving Emission-superfast model

--- ext/EmissionExt.jl
+++ ext/EmissionExt.jl
@@ -11,12 +11,12 @@ lev = 1.0
 
 Base.:(+)(emis::NEI2016MonthlyEmis, sf::SuperFast) = operator_compose(sf, emis, Dict(
     sf.sys.NO2 => emis.sys.mrggrid_withbeis_withrwc₊NO2 => uu * 2000 / 2240 * 1e15 * R * Tep / (Δz2 * 46.0055 * P * 3600 * 24),
-    s.sys.NO => emis.sys.mrggrid_withbeis_withrwc₊NO => uu * 2000 / 2240 * 1e15 * R * Tep / (Δz2 * 46.0055 * P * 3600 * 24),
-    s.sys.CH2O => emis.sys.mrggrid_withbeis_withrwc₊FORM => uu * 2000 / 2240 * 1e15 * R * Tep / (Δz2 * 46.0055 * P * 3600 * 24),
-    s.sys.CH4 => emis.sys.mrggrid_withbeis_withrwc₊CH4 => uu * 2000 / 2240 * 1e15 * R * Tep / (Δz2 * 46.0055 * P * 3600 * 24),
-    s.sys.CO => emis.sys.mrggrid_withbeis_withrwc₊CO => uu * 2000 / 2240 * 1e15 * R * Tep / (Δz2 * 46.0055 * P * 3600 * 24),
-    s.sys.SO2 => emis.sys.mrggrid_withbeis_withrwc₊SO2 => uu * 2000 / 2240 * 1e15 * R * Tep / (Δz2 * 46.0055 * P * 3600 * 24),
-    s.sys.ISOP => emis.sys.mrggrid_withbeis_withrwc₊ISOP => uu * 2000 / 2240 * 1e15 * R * Tep / (Δz2 * 46.0055 * P * 3600 * 24)
+    sf.sys.NO => emis.sys.mrggrid_withbeis_withrwc₊NO => uu * 2000 / 2240 * 1e15 * R * Tep / (Δz2 * 46.0055 * P * 3600 * 24),
+    sf.sys.CH2O => emis.sys.mrggrid_withbeis_withrwc₊FORM => uu * 2000 / 2240 * 1e15 * R * Tep / (Δz2 * 46.0055 * P * 3600 * 24),
+    sf.sys.CH4 => emis.sys.mrggrid_withbeis_withrwc₊CH4 => uu * 2000 / 2240 * 1e15 * R * Tep / (Δz2 * 46.0055 * P * 3600 * 24),
+    sf.sys.CO => emis.sys.mrggrid_withbeis_withrwc₊CO => uu * 2000 / 2240 * 1e15 * R * Tep / (Δz2 * 46.0055 * P * 3600 * 24),
+    sf.sys.SO2 => emis.sys.mrggrid_withbeis_withrwc₊SO2 => uu * 2000 / 2240 * 1e15 * R * Tep / (Δz2 * 46.0055 * P * 3600 * 24),
+    sf.sys.ISOP => emis.sys.mrggrid_withbeis_withrwc₊ISOP => uu * 2000 / 2240 * 1e15 * R * Tep / (Δz2 * 46.0055 * P * 3600 * 24)
 ))
 
 Base.:(+)(b::SuperFast, e::NEI2016MonthlyEmis) = e + b
