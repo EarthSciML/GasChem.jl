6a32a92488767b25009a2e659773014da9db6696
jialinl6 <jialinl6@illinois.edu>
2025-03-24 16:27:28 -0500

    Debug test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,19 +2,19 @@ using GasChem
 using Test, SafeTestsets
 
 @testset "GasChem.jl" begin
-    @safetestset "SuperFast" begin
-         include("superfast_test.jl")
-    end
+    # @safetestset "SuperFast" begin
+    #      include("superfast_test.jl")
+    # end
     @safetestset "FastJX" begin
         include("fastjx_test.jl")
     end
-    @safetestset "compose_fastjx_superfast" begin
-        include("compose_fastjx_superfast_test.jl")
-    end
-    @safetestset "geoschem_test" begin
-         include("geoschem_test.jl")
-    end
-    @safetestset "EarthSciData coupling" begin
-         include("EarthSciData_test.jl")
-    end
+    # @safetestset "compose_fastjx_superfast" begin
+    #     include("compose_fastjx_superfast_test.jl")
+    # end
+    # @safetestset "geoschem_test" begin
+    #      include("geoschem_test.jl")
+    # end
+    # @safetestset "EarthSciData coupling" begin
+    #      include("EarthSciData_test.jl")
+    # end
 end
